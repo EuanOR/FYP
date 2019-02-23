d9dee5b7cdba18509deab993963689348c814a1f
Euan O'Regan <115312821@umail.ucc.ie>
2019-02-23 19:14:06 +0000

    Added main loop and change functionality

--- Controller.py
+++ Controller.py
@@ -1,6 +1,13 @@
+from House import House
+from Room import Room
+from Rad import Rad
+from WeatherAPI import WeatherAPI
+import time
 
 class Controller(object):
-    
+
+    api = WeatherAPI()
+
     def __init__(self, house, monitor):
         
         self._house = house
@@ -21,3 +28,36 @@ class Controller(object):
     def setMonitor(self, newMonitor):
 
         self._monitor = newMonitor
+    
+    def calcIncrease(self, room):
+        
+        increase = 0.0
+        if room.getRad().isActive():
+            increase = room.rad.getBTU() / room.getArea()
+        
+        return increase
+    
+    def calcDecrease(self, room):
+        
+        decrease = 0.0 
+        return decrease
+
+    def mainLoop(self):
+        
+        pollTime = time.perf_counter()
+        changeTime = time.perf_counter()
+        
+        while True:
+            
+            if (time.perf_counter() - pollTime) > 10.0:
+                self._monitor.monitor()
+                pollTime = time.perf_counter()
+            
+            if (time.perf_counter() - changeTime) > 30.0:
+                for r in self._house.getRooms():
+                    increase = self.calcIncrease(r)
+                    decrease = self.calcDecrease(r)
+                    change = increase - decrease
+                    r.changeTemp(change)
+                
+                changeTime = time.perf_counter()
