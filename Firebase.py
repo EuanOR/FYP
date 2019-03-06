9106f8ea2ed629801736b1593a4a164fbc54987b
Euan O'Regan <115312821@umail.ucc.ie>
2019-03-06 22:09:22 +0000

    Added light automation checker

--- Firebase.py
+++ Firebase.py
@@ -10,18 +10,18 @@ firebase = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://homecomfortscontrol.firebaseio.com/'
 })
 # As an admin, the app has access to read and write all data, regradless of Security Rules
-house = db.reference('House')
+house = db.reference('house')
 
 
 def get_heating_active():
 
-    active = house.child('Heating').child('Active')
+    active = house.child('heating').child('active')
     return active.get()
 
 
 def set_heating_active(state):
 
-    active = house.child('Heating').child('Active')
+    active = house.child('heating').child('active')
 
     if isinstance(state, bool):
         active.set(state)
@@ -29,19 +29,48 @@ def set_heating_active(state):
         print("Heating state must be a boolean value. Received" + str(state))
 
 
+def get_heating_threshold():
+
+    threshold = house.child('heating').child('threshold')
+    return threshold.get()
+
+
+def heating_automated():
+
+    automated = house.child('heating').child('automated')
+    return automated
+
+
 def get_lights_active():
 
-    active = house.child('Lights').child('Active')
+    active = house.child('lights').child('active')
 
     return active.get()
 
 
 def set_lights_active(state):
 
-    active = house.child('Lights').child('Active')
+    active = house.child('lights').child('active')
 
     if isinstance(state, bool):
         active.set(state)
     else:
         print("Lights state must be a boolean value. Received" + str(state))
 
+
+def get_lights_start():
+
+    start_time = house.child('lights').child('start').get()
+    return start_time
+
+
+def get_lights_end():
+
+    end_time = house.child('lights').child('end').get()
+    return end_time
+
+
+def lights_automated():
+
+    automated = house.child('lights').child('automated')
+    return automated
